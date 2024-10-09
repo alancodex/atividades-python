b6a5df090ffe8c76ac689140ea2f06780f331fff
alancodex <alansobraljs@gmail.com>
2024-10-09 17:30:21 -0300

    adicionais 1.1

---
+++ campo.py
@@ -0,0 +1,7 @@
+altura = float(input("Digite a altura do campo: "))
+largura = float(input("Digite a largura do campo: "))
+
+area_terreno = altura * largura 
+
+print(f"A área do terreno é: {area_terreno} hectares")
+print(f"Será necessario: {area_terreno/5} rolos de grama para cobrir o campo")
---
+++ mediaponderada.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite a primeira nota do aluno: "))
+n2 = float(input("Digite a segunda nota do aluno: "))
+n3 = float(input("Digite a terceira nota do aluno: "))
+
+media = ((n1 *2) + (n2 * 3) + (n3 * 5)) / 10
+
+print(f"A média das notas do aluno é: {media}")
